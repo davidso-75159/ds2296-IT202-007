403a9332b2aadf1858b1fd32499f548b5a25a594
davidso-75159 <ds2296@njit.edu>
2024-09-23 15:20:48 -0400

    Solution: Loop through each array, check if not divisible by 2, then print if true

--- public_html/php_hw_mod2/problem1.php
+++ public_html/php_hw_mod2/problem1.php
@@ -10,6 +10,11 @@ function processArray($arr) {
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
     //TODO add logic here to echo out only odd values
     //end edits
+    foreach ($arr as $num) {
+        if ($num % 2 == 1) {
+            echo $num;
+        }
+    }
 }
 echo "Problem 1: Odd Output<br>";
 ?>
