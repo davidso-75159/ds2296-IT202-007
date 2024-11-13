cf62f865cc39a22e014d43bb31bcdf437c5fa005
David So <ds2296@njit.edu>
2024-11-13 11:14:41 -0500

    LogoutMessage

--- public_html/project/logout.php
+++ public_html/project/logout.php
@@ -2,7 +2,8 @@
 session_start();
 session_unset();
 session_destroy();
+session_start();
 require(__DIR__."/../../lib/functions.php");
-flash("You have been logged out.");
+flash("You have been logged out.", "success");
 header("Location: login.php");
 ?>
