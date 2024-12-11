a86341a3283b820cf7d623a429272b700426d9a8
David So <ds2296@njit.edu>
2024-12-11 15:58:20 -0500

    SingleDriverView

--- public_html/project/view_driver.php
+++ public_html/project/view_driver.php
@@ -22,10 +22,25 @@ if ($id > 0) {
 }
 ?>
 
-<?php if($driver):?>
+<div class="container-fluid">
+    <h3>Driver: <?php se($driver, 'firstName'.' '.'lastName', "No Name"); ?></h3>
     <div>
-        <?php render_table($driver); ?>
+        <a href="<?php echo get_url("project/list_drivers.php"); ?>" class="btn btn-secondary">Back</a>
     </div>
-<?php endif;?>
+    <div class="card mx-auto" style="width: 18rem;">
+        <img src="https://www.fia.com/sites/default/files/styles/panopoly_image_original/public/fia_logo_final_.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title"><?php se($driver, 'firstName'.' '.'lastName', "No Name");?></h5>
+            <div class="card-text">
+                <ul class="list-group">
+                    <li class="list-group-item">Number: <?php se($driver, "number", "Unknown"); ?></li>
+                    <li class="list-group-item">3-letter code: <?php se($driver, "code", "Unknown"); ?></li>
+                    <li class="list-group-item">Nationality: <?php se($driver, "nationality", "Unknown"); ?></li>
+                    <li class="list-group-item">Date of Birth: <?php se($driver, "birthday", "Unknown"); ?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 <?php 
 require_once(__DIR__ . "/../../partials/flash.php");
